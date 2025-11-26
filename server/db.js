86597646653f2ceee83080759d139122c446d523
Alezito2008 <alejandro.kretzig@gmail.com>
2025-11-26 19:03:18 -0300

    Arreglado package.json

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import Database from 'better-sqlite3';
 
-const DB_FILE = process.env.DB_FILE || 'data.db';
+const DB_FILE = process.env.DB_FILE || 'excalisaver.db';
 
 const db = new Database(DB_FILE);
 
